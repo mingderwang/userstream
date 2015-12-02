7876997cb638856c7501a00242c5e5d082d6acb5
Ming-der Wang <ming@log4analytics.com>
2015-12-02 19:35:20 +0800

    modify for auto twitter follow back by user id

--- client.go
+++ client.go
@@ -2,7 +2,12 @@ package userstream
 
 import (
 	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/mrjones/oauth"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -12,6 +17,12 @@ var (
 	requestTokenUrl     = "https://api.twitter.com/oauth/request_token"
 	authorizeTokenUrl   = "https://api.twitter.com/oauth/authorize"
 	accessTokenUrl      = "https://api.twitter.com/oauth/access_token"
+
+	twitterApiUrl = "https://api.twitter.com"
+
+	requestTokenPath   = "/oauth/request_token"
+	authorizeTokenPath = "/oauth/authorize"
+	accessTokenPath    = "/oauth/access_token"
 )
 
 type Client struct {
@@ -19,10 +30,32 @@ type Client struct {
 	ConsumerSecret    string
 	AccessToken       string
 	AccessTokenSecret string
+	cons              *oauth.Consumer
+	token             *oauth.AccessToken
 }
 
 func (c *Client) UserStream(callback func(interface{})) {
 	c.connect(userStreamsEndPoint, callback)
+}
+
+func (c *Client) FollowUserId(tweetId int64) (*UserDetails, error) {
+	user := UserDetails{}
+	id := fmt.Sprintf("%d", tweetId)
+	url := fmt.Sprintf("/1.1/friendships/create.json")
+	fmt.Println(url)
+	fmt.Println(id)
+	response, err := c.post(
+		c.apiUrl(url),
+		map[string]string{
+			"user_id": "1401881",
+			"follow":  "true",
+		},
+	)
+	if err != nil {
+		return &user, err
+	}
+
+	return c.userDetailsByResponse(response)
 }
 
 func (c *Client) connect(endPoint string, callback func(interface{})) {
@@ -64,4 +97,58 @@ func (c *Client) accessToken() *oauth.AccessToken {
 		Token:  c.AccessToken,
 		Secret: c.AccessTokenSecret,
 	}
+
+}
+
+func (c *Client) get(requestUrl string, params map[string]string) (*http.Response, error) {
+	return c.consumer().Get(requestUrl, params, c.accessToken())
+}
+
+func (c *Client) post(requestUrl string, params map[string]string) (*http.Response, error) {
+	return c.consumer().Post(requestUrl, params, c.accessToken())
+}
+
+func (c *Client) consumer() *oauth.Consumer {
+	if c.cons != nil {
+		return c.cons
+	}
+
+	c.cons = oauth.NewConsumer(
+		c.ConsumerKey,
+		c.ConsumerSecret,
+		oauth.ServiceProvider{
+			RequestTokenUrl:   c.apiUrl(requestTokenPath),
+			AuthorizeTokenUrl: c.apiUrl(authorizeTokenPath),
+			AccessTokenUrl:    c.apiUrl(accessTokenPath),
+		},
+	)
+	return c.cons
+}
+
+func (c *Client) apiUrl(format string, a ...interface{}) string {
+	apiPath := fmt.Sprintf(format, a...)
+	return twitterApiUrl + apiPath
+}
+
+func (c *Client) userDetailsByResponse(response *http.Response) (*UserDetails, error) {
+	decoder := c.jsonDecoder(response)
+	user := UserDetails{}
+	decoder.Decode(&user)
+	spew.Dump(user)
+	return &user, nil
+}
+
+func (c *Client) tweetsByResponse(response *http.Response) ([]Tweet, error) {
+	decoder := c.jsonDecoder(response)
+	tweets := []Tweet{}
+	decoder.Decode(&tweets)
+	return tweets, nil
+}
+
+func (c *Client) jsonDecoder(response *http.Response) *json.Decoder {
+	data, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return json.NewDecoder(bytes.NewReader(data))
 }
